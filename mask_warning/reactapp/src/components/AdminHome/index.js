1c24a424add36261e399cc2161bf0cf72706edb6
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-04-04 14:23:12 +0700

    home admin statistic

--- mask_warning/reactapp/src/components/AdminHome/index.js
+++ mask_warning/reactapp/src/components/AdminHome/index.js
@@ -1,11 +1,27 @@
 import React, { useRef, useState } from "react";
 import styles from "./Home.module.css"
 import { Link } from "react-router-dom";
-
+import StatisticCard from "../AdminStatisticCard"
+import UsersManagerAdmin from "../AdminUsersManager"
+import ReportsManagerAdmin from "../AdminReportsManager"
 
 const HomeAdmin = () => {
 
     const [toggle, setToggle] = useState("home")
+
+    const [statisticToggle, setStatisticToggle] = useState("week")
+
+    const dataUsersStatistic = {
+        "week": 6,
+        "month": 23,
+        "year": 153
+    }
+
+    const dataAvarageHoursStatistic = {
+        "week": 15,
+        "month": 22,
+        "year": 20
+    }
 
     return (
         <section className={styles.homeMain}>
@@ -34,7 +50,7 @@ const HomeAdmin = () => {
                 </div>
 
                 <div className={styles.homeDecs}>
-                    <img src="./icons/line.png"/>
+                    <img src="./icons/line.png" />
                     <h3>Mask Warning Admin Website</h3>
                     <p>Mask Warning Admin Website is a system help store notify customer wearing mask</p>
                 </div>
@@ -42,14 +58,48 @@ const HomeAdmin = () => {
             </div>
 
             <div className={styles.homeTabContents}>
+                <div className={styles.homeTopRightControl}>
+                    <p>Admin</p>
+                    <img src="./icons/bell.png" />
+                    <img src="./icons/dashicons-migrate.png" />
+                </div>
                 <div className={toggle === "home" ? `${styles.homeTabContent} ${styles.active}` : `${styles.homeTabContent}`}>
 
+                    <div className={styles.homeStatisticButtonGroup}>
+                        <div className={statisticToggle === "week" ? `${styles.homeStatisticButton} ${styles.active}` :
+                            styles.homeStatisticButton} onClick={() => setStatisticToggle("week")}>
+                            <img src="./icons/ellipse.png" />
+                            <p>Week</p>
+                        </div>
+                        <div className={statisticToggle === "month" ? `${styles.homeStatisticButton} ${styles.active}` :
+                            styles.homeStatisticButton} onClick={() => setStatisticToggle("month")}>
+                            <img src="./icons/ellipse.png" />
+                            <p>Month</p>
+                        </div>
+                        <div className={statisticToggle === "year" ? `${styles.homeStatisticButton} ${styles.active}` :
+                            styles.homeStatisticButton} onClick={() => setStatisticToggle("year")}>
+                            <img src="./icons/ellipse.png" />
+                            <p>Year</p>
+                        </div>
+                    </div>
+                    <div className={styles.homeStatisticCards}>
+                        <StatisticCard cardName={"User(s)"} data={dataUsersStatistic[statisticToggle]}
+                            dataType={" new user(s)"} iconLink={"./icons/new_users.png"} />
+                        <StatisticCard cardName={"Average hour(s) per day"} data={dataAvarageHoursStatistic[statisticToggle]}
+                            dataType={" hour(s) per day"} iconLink={"./icons/average_hours.png"} />
+                    </div>
                 </div>
 
-                
+                <div className={toggle === "users" ? `${styles.homeTabContent} ${styles.active}` : `${styles.homeTabContent}`}>
+                    <UsersManagerAdmin />
+                </div>
+
+                <div className={toggle === "reports" ? `${styles.homeTabContent} ${styles.active}` : `${styles.homeTabContent}`}>
+                    <ReportsManagerAdmin />
+                </div>
 
             </div>
-        </section>
+        </section >
     )
 }
 export default HomeAdmin;
