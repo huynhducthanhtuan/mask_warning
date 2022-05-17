1be207e24a1220b564603b9b614deb5000d5b27c
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-05-18 01:09:33 +0700

    fix bug cam

---
+++ mask_warning/mask_warning/detect_mask_video.py
@@ -0,0 +1,157 @@
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+import pandas as pd
+
+# function to calculate minimum distance from all colors and get the most matching color
+def get_color_name(R, G, B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
+    return cname
+
+
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+def detect_and_predict_mask(frame, maskNet):
+    
+    faces = []
+    locs = []
+    preds = []
+
+    face_rects = face_cascade.detectMultiScale(frame) 
+    
+    for (x,y,wf,hf) in face_rects: 
+        (startX, startY, endX, endY) = (x,y,x+wf,y+hf)
+        # ensure the bounding boxes fall within the dimensions of
+        # the frame
+        (startX, startY) = (max(0, startX), max(0, startY))
+        (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+        print(startX,startY,endX,endY)
+        # extract the face ROI, convert it from BGR to RGB channel
+        # ordering, resize it to 224x224, and preprocess it
+        face = frame[startY:endY, startX:endX]
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        # cv2.imshow('face',face)
+        face = cv2.resize(face, (224, 224))
+        face = img_to_array(face)
+        face = preprocess_input(face)
+
+        # add the face and bounding boxes to their respective
+        # lists
+        faces.append(face)
+        locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
+
+def detectColor(frame,x,y):
+	global h,w
+
+	if y >= h or x >= w:
+		return frame
+
+	b, g, r = frame[y, x]
+	b = int(b)
+	g = int(g)
+	r = int(r)
+
+	cv2.circle(frame, (x,y),2,color,-1)
+	# cv2.rectangle(image, start point, endpoint, color, thickness)-1 fills entire rectangle
+	# cv2.rectangle(frame, (20, 20), (750, 60), (b, g, r), -1)
+
+	# Creating text string to display( Color name and RGB values )
+	text = get_color_name(r, g, b) + ' R=' + str(r) + ' G=' + str(g) + ' B=' + str(b)
+
+	# cv2.putText(frame,text,start,font(0-7),fontScale,color,thickness,lineType )
+	# cv2.putText(frame, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+	# For very light colours we will display text in black colour
+	# if r + g + b >= 600:
+		# cv2.putText(frame, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
+	return text
+
+
+# Load csv color file
+index = ["color_name","R","G","B"]
+csv = pd.read_csv('./maMau4.csv', names=index, header=None)
+
+# load our serialized face detector model from disk
+prototxtPath = r"face_detector\deploy.prototxt"
+weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+maskNet = load_model("mask_detector.model")
+
+# initialize the video stream
+print("[INFO] starting video stream...")
+vs = cv2.VideoCapture('rtsp://192.168.1.45:554/cam/realmonitor?channel=1&subtype=0&unicast=true&proto=Onvif')
+frame = vs.read()
+h,w = frame.shape[:2]
+zoomRate = w / float(400) 
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
+	ret,frame = vs.read()
+
+	# detect faces in the frame and determine if they are wearing a
+	# face mask or not
+	(locs, preds) = detect_and_predict_mask(imutils.resize(frame,width=400), faceNet, maskNet)
+
+	# loop over the detected face locations and their corresponding
+	# locations
+	for (box, pred) in zip(locs, preds):
+		# unpack the bounding box and predictions
+		(startX, startY, endX, endY) = box
+		(startX, startY, endX, endY) = (np.int32(startX*zoomRate), np.int32(startY*zoomRate), np.int32(endX*zoomRate), np.int32(endY*zoomRate))
+		(mask, withoutMask) = pred
+
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "Mask" if mask > withoutMask else "No Mask"
+		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+		colorDetec = detectColor(frame,np.int32(startX+(endX-startX)/2),np.int32(endY+(endY-startY)/2))
+		# include the probability in the label
+		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+		label += f'-{colorDetec}' 
+		# display the label and bounding box rectangle on the output
+		# frame
+		cv2.putText(frame, label, (startX, startY - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
--- mask_warning/mask_warning/views.py
+++ mask_warning/mask_warning/views.py
@@ -10,56 +10,86 @@ from imutils.video import VideoStream
 from .apis import GetVideoStreamUrl
 import threading, imutils, time, cv2, os, numpy as np
 from threading import Thread
+import pandas as pd
+
+index = ["color_name","R","G","B"]
+csv = pd.read_csv('./maMau4.csv', names=index, header=None)
+h,w = 0,0
 
 
-def detect_and_predict_mask(frame, faceNet, maskNet, zoomRate):
-    # grab the dimensions of the frame and then construct a blob
-    # from it
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
-        (104.0, 177.0, 123.0))
 
-    # pass the blob through the network and obtain the face detections
-    faceNet.setInput(blob)
-    detections = faceNet.forward()
+def get_color_name(R, G, B):
+    minimum = 10000
+    cname = 'undefined'
+    r,g,b = 0,0,0
+    for i in range(len(csv)):
+        r = R - int(csv.loc[i, "R"])
+        g = G - int(csv.loc[i, "G"])
+        b = B - int(csv.loc[i, "B"])
+        d = np.sqrt(r**2+b**2+g**2)
+        if d <= minimum:
+            minimum = d
+            cname = csv.loc[i, "color_name"]
 
-    # initialize our list of faces, their corresponding locations,
-    # and the list of predictions from our face mask network
+    return cname
+
+def detectColor(frame,x,y,color):
+    h,w = frame.shape[:2]
+
+    if y >= h or x >= w:
+        return 'undefined'
+
+    b, g, r = frame[y, x]
+    b = int(b)
+    g = int(g)
+    r = int(r)
+
+    # cv2.circle(frame, (x,y),3,color,-1)
+    # cv2.rectangle(image, start point, endpoint, color, thickness)-1 fills entire rectangle
+    # cv2.rectangle(frame, (20, 20), (750, 60), (b, g, r), -1)
+
+    # Creating text string to display( Color name and RGB values )
+    text = get_color_name(r, g, b)
+
+    # cv2.putText(frame,text,start,font(0-7),fontScale,color,thickness,lineType )
+    # cv2.putText(frame, text, (50, 50), 2, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
+
+    # For very light colours we will display text in black colour
+    # if r + g + b >= 600:
+        # cv2.putText(frame, text, (50, 50), 2, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
+
+    return text
+face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+def predict_mask(frame, maskNet):
+    h,w = frame.shape[:2]    
     faces = []
     locs = []
     preds = []
 
-    # loop over the detections
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the detection
-        confidence = detections[0, 0, i, 2]
+    face_rects = face_cascade.detectMultiScale(frame) 
+    
+    for (x,y,wf,hf) in face_rects: 
+        (startX, startY, endX, endY) = (x,y,x+wf,y+hf)
+        if(wf*hf <= 10000):
+            continue
+        # ensure the bounding boxes fall within the dimensions of
+        # the frame
+        (startX, startY) = (max(0, startX), max(0, startY))
+        (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+        # extract the face ROI, convert it from BGR to RGB channel
+        # ordering, resize it to 224x224, and preprocess it
+        face = frame[startY:endY, startX:endX]
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        # cv2.imshow('face',face)
+        face = cv2.resize(face, (224, 224))
+        face = img_to_array(face)
+        face = preprocess_input(face)
 
-        # filter out weak detections by ensuring the confidence is
-        # greater than the minimum confidence
-        if confidence > 0.5:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-
-            # ensure the bounding boxes fall within the dimensions of
-            # the frame
-            (startX, startY) = (max(0, startX), max(0, startY))
-            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
-
-            # extract the face ROI, convert it from BGR to RGB channel
-            # ordering, resize it to 224x224, and preprocess it
-            face = frame[startY:endY, startX:endX]
-            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
-            face = cv2.resize(face, (224, 224))
-            face = img_to_array(face)
-            face = preprocess_input(face)
-
-            # add the face and bounding boxes to their respective
-            # lists
-            faces.append(face)
-            locs.append((startX, startY, endX, endY))
+        # add the face and bounding boxes to their respective
+        # lists
+        faces.append(face)
+        locs.append((startX, startY, endX, endY))
 
     # only make a predictions if at least one face was detected
     if len(faces) > 0:
@@ -69,7 +99,8 @@ def detect_and_predict_mask(frame, faceNet, maskNet, zoomRate):
         faces = np.array(faces, dtype="float32")
         preds = maskNet.predict(faces, batch_size=32)
 
-    # return a 2-tuple of the face locations and their corresponding locations
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
     return (locs, preds)
 
 # # load our serialized face detector model from disk
@@ -89,11 +120,23 @@ pts = np.array([[15,15], [625, 15],
 pts = pts.reshape((-1, 1, 2))
 
 # Frame time when red corner off
-redCornerOffFrame = [7,8,9]
+redCornerOffFrame = [5,6]
 
-def playUnmaskSound(urlSound):
-    # using play sound
-    playsound(urlSound)
+prefix = fr"{os.getcwd()}\colorSound\prefix.mp3"
+posfix = fr"{os.getcwd()}\colorSound\posfix.mp3"
+
+def playUnmaskSound(unmaskColors):
+
+    if unmaskColors == ['undefined']:
+        playsound(fr"{os.getcwd()}\colorSound\undefined.mp3")
+        return
+    unmaskColors = list(dict.fromkeys(unmaskColors))
+    print(unmaskColors)
+    playsound(prefix)
+    for color in unmaskColors:
+        if color not in'undefined':
+            playsound(fr"{os.getcwd()}\colorSound\{color}.mp3")
+    playsound(posfix)
 
 
 def stream(videoStreamUrl):
@@ -103,91 +146,98 @@ def stream(videoStreamUrl):
     threaded_camera = ThreadedCamera(videoStreamUrl) 
     frame_count = 0
 
-    limitCallSound = 22;
+    limitCallSound = 150
     lastFrameCallSound = 0
-    urlSound = fr"{os.getcwd()}\vuilongdeokhautrang.mp3"
+    limitDetected = 100
+    lastFrameDetect = 0
+    isOnRedcorner = False
+    lastFrameRedCorner = 0
+    stopFrameRedCorner = 0
     while True:
-        frame_count += 1
-        frame = threaded_camera.show_frame()
-        (h,w) = frame.shape[:2]
 
-        # print('----------------------\n',h,w)
-        # frame = imutils.resize(frame,width=400)
-        # (h,w) = frame.shape[:2]
-        # print(h,w,'-----------------------\n')
-        zoomRate = float(400) / w
-        # ret,frame = cap.read()
-        (locs, preds) = detect_and_predict_mask(imutils.resize(frame,width=400), faceNet, maskNet, zoomRate)
+        try:
+            frame,frame_count = threaded_camera.show_frame()
+        except:
+            print(frame_count,'-fail')
+            continue
 
-        # loop over the detected face locations and their corresponding
-        # locations
-        for (box, pred) in zip(locs, preds):
-            # unpack the bounding box and predictions
-            (startX, startY, endX, endY) = box
-            (startX, startY, endX, endY) = (np.int32(startX*3.2), np.int32(startY*3.2), np.int32(endX*3), np.int32(endY*3))
-            # print(startX, startY, endX, endY)
-            (mask, withoutMask) = pred
+        h,w = frame.shape[:2]
+        if (frame_count - lastFrameDetect >= limitDetected) or (lastFrameDetect == 0):
+            lastFrameDetect = frame_count
+            (locs, preds) = predict_mask(frame,maskNet)
 
-            # determine the class label and color we'll use to draw
-            # the bounding box and text
-            # and put red corner if need
-            if mask > withoutMask:
-                label = 'Mask'
-                color = (0, 255, 0)
-                frame_count = 0
+            # loop over the detected face locations and their corresponding
+            # locations
+            unmaskColors = []
 
-            else:
-                label = 'No Mask'
-                color = (0, 0, 255)
+            for (box, pred) in zip(locs, preds):
+                # unpack the bounding box and predictions
+                (startX, startY, endX, endY) = box
+                # print(startX, startY, endX, endY)
+                (mask, withoutMask) = pred
 
-                isOnRedcorner = True
+                # determine the class label and color we'll use to draw
+                # the bounding box and text
+                # and put red corner if need
+                if mask > withoutMask:
+                    label = 'Mask'
+                    color = (0, 255, 0)
 
-            for time_off in redCornerOffFrame:
-                if frame_count % time_off == 0:
+                else:
+                    label = 'No Mask'
+                    color = (0, 0, 255)
+                    isOnRedcorner = True
+
+                
+                
+                # print(h,w)
+                    
+                #     frame = cv2.polylines(frame, [pts], isClosed=True, color=color, thickness=30)
+
+                shirtColor = detectColor(frame,np.int32(startX+(endX-startX)/2),np.int32(endY+(endY-startY)/1.7),color)
+
+                # print(frame_count,lastFrameCallSound)
+                if label == 'No Mask':
+                    unmaskColors.append(shirtColor)
+
+
+                # include the probability in the label
+                label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+                label += f'-{shirtColor}'
+                
+
+
+                # display the label and bounding box rectangle on the output
+                # frame
+                # cv2.putText(frame, label, (startX, startY - 10),
+                #     cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+                cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+            if unmaskColors != [] :
+                if stopFrameRedCorner < frame_count:
+                    stopFrameRedCorner = frame_count + 200
+
+                if frame_count - lastFrameCallSound >= limitCallSound:
+                    lastFrameCallSound = frame_count
+                    t2 = threading.Thread(target=playUnmaskSound, args=(unmaskColors,))
+                    t2.start()
+        
+        # print(stopFrameRedCorner,frame_count)
+        if stopFrameRedCorner >= frame_count:
+            lastFrameRedCorner = frame_count
+            for offTime in redCornerOffFrame:
+                if lastFrameRedCorner % offTime == 0:
                     isOnRedcorner = False
                     break
-            
-            # print(h,w)
             if isOnRedcorner:
-                cv2.rectangle(frame, (0, 0), (w, h), color, 40)
-                
-            #     frame = cv2.polylines(frame, [pts], isClosed=True, color=color, thickness=30)
-
-            if label == 'No Mask' and lastFrameCallSound == 0 or frame_count - lastFrameCallSound >= limitCallSound:
-                lastFrameCallSound = frame_count
-
-                # using play sound
-                # playsound(urlSound)
-
-                # using vlc
-                # p = vlc.Me
-                # diaPlayer(urlSound)
-                # p.play()
-                # time.sleep(3)
-                # p.stop()
-                t2 = threading.Thread(target=playUnmaskSound, args=(urlSound,))
-                t2.start()
-
-
-            # include the probability in the label
-            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-
-            
-
-
-            # display the label and bounding box rectangle on the output
-            # frame
-            cv2.putText(frame, label, (startX, startY - 10),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-        
-        
-
+                cv2.rectangle(frame, (0, 0), (w, h), (0, 0, 255), 20)  
+            isOnRedcorner = True
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
         cv2.imwrite('demo.jpg', frame)
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + open('demo.jpg', 'rb').read() + b'\r\n')
-        # yield (b'--frame\r\n'
-        #         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def showCamera(request, userId):
     try:
@@ -207,10 +257,12 @@ class ThreadedCamera(object):
     def __init__(self, src=0):
         self.capture = cv2.VideoCapture(src)
         self.capture.set(cv2.CAP_PROP_BUFFERSIZE, 2)
-       
+        self.frameCount = 0
+        (self.status, self.frame) = self.capture.read()
+        h,w = self.frame.shape[:2]
         # FPS = 1/X
         # X = desired FPS
-        self.FPS = 1/40
+        self.FPS = 1/50
         self.FPS_MS = int(self.FPS * 1000)
         
         # Start frame retrieval thread
@@ -221,12 +273,13 @@ class ThreadedCamera(object):
     def update(self):
         while True:
             if self.capture.isOpened():
+                self.frameCount += 1
                 (self.status, self.frame) = self.capture.read()
             time.sleep(self.FPS)
             
     def show_frame(self):
         cv2.waitKey(self.FPS_MS)
-        return self.frame
+        return self.frame,self.frameCount
         # image = self.frame
         # _, jpeg = cv2.imencode('.jpg', image)
         # return jpeg.tobytes()
